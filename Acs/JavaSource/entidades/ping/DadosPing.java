da9f0b90765ee91f843dbcd419f1cf734e083cc0
FabioClem <fabio.henrique.clem@gmail.com>
2016-11-07 10:08:28 -0200

    Daily Update

--- Acs/JavaSource/entidades/ping/DadosPing.java
+++ Acs/JavaSource/entidades/ping/DadosPing.java
@@ -5,7 +5,21 @@ public class DadosPing {
 	private String hostAdress = "www.google.com";
 	
 	private Integer numberRepetitions = 4;
-	
-	
 
+	public String getHostAdress() {
+		return hostAdress;
+	}
+
+	public void setHostAdress(String hostAdress) {
+		this.hostAdress = hostAdress;
+	}
+
+	public Integer getNumberRepetitions() {
+		return numberRepetitions;
+	}
+
+	public void setNumberRepetitions(Integer numberRepetitions) {
+		this.numberRepetitions = numberRepetitions;
+	}
+	
 }
