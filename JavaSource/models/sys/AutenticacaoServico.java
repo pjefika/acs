611e09af44abc902f96c3ed3d79fc3a2f4b89d0e
henmerlin <henmerlin@gmail.com>
2017-04-25 17:20:21 -0300

    Force EndPoint

--- JavaSource/models/sys/AutenticacaoServico.java
+++ JavaSource/models/sys/AutenticacaoServico.java
@@ -1,80 +1,85 @@
 package models.sys;
 
+import entidades.sys.Autenticacao;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import entidades.sys.Autenticacao;
-
 @Stateless
 public class AutenticacaoServico {
 
-	@PersistenceContext(unitName="vu")
-	private EntityManager entityManager;
-	
-	public void cadastrarAutenticacao(Autenticacao autenticacao) throws Exception {
-		
-		try {
-			
-			this.entityManager.persist(autenticacao);
-			
-		} catch (Exception e) {
+    @PersistenceContext(unitName = "vu")
+    private EntityManager entityManager;
 
-			throw new Exception("Erro ao cadastrar Autenticação.");
-			
-		}
-		
-	}
-	
-	public void modificarAutenticacao(Autenticacao autenticacao) throws Exception {
-		
-		try {
-			
-			this.entityManager.merge(autenticacao);
-			
-		} catch (Exception e) {
-			
-			throw new Exception("Erro ao modificar Autenticação");
-			
-		}
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Autenticacao> listarTodasAutenticacao() {
-		
-		try {
-			
-			Query query = this.entityManager.createQuery("FROM Autenticacao a");
-			return query.getResultList();
-			
-		} catch (Exception e) {
+    public void cadastrarAutenticacao(Autenticacao autenticacao) throws Exception {
 
-			return new ArrayList<Autenticacao>();
-			
-		}
-		
-	}
-	
-	public Autenticacao listarAutenticacaoAtiva() {
-		
-		try {
-						
-			Query query = this.entityManager.createQuery("FROM Autenticacao a WHERE a.ativo =:param1");
-			query.setParameter("param1", true);
-			query.setMaxResults(1);
-			return (Autenticacao) query.getSingleResult();
-			
-		} catch (Exception e) {
-			
-			return new Autenticacao();
-			
-		}
-		
-	}
-	
+        try {
+
+            this.entityManager.persist(autenticacao);
+
+        } catch (Exception e) {
+
+            throw new Exception("Erro ao cadastrar Autenticação.");
+
+        }
+
+    }
+
+    public void modificarAutenticacao(Autenticacao autenticacao) throws Exception {
+
+        try {
+
+            this.entityManager.merge(autenticacao);
+
+        } catch (Exception e) {
+
+            throw new Exception("Erro ao modificar Autenticação");
+
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Autenticacao> listarTodasAutenticacao() {
+
+        try {
+
+            Query query = this.entityManager.createQuery("FROM Autenticacao a");
+            return query.getResultList();
+
+        } catch (Exception e) {
+
+            return new ArrayList<Autenticacao>();
+
+        }
+
+    }
+
+    public Autenticacao listarAutenticacaoAtiva() {
+//
+//		try {
+//
+//			Query query = this.entityManager.createQuery("FROM Autenticacao a WHERE a.ativo =:param1");
+//			query.setParameter("param1", true);
+//			query.setMaxResults(1);
+//			return (Autenticacao) query.getSingleResult();
+//
+//		} catch (Exception e) {
+//
+//			return new Autenticacao();
+//
+//		}
+        Autenticacao a = new Autenticacao();
+
+        a.setAtivo(Boolean.TRUE);
+        a.setLink("http://10.200.6.150/nbbs/api/");
+        a.setUser("efika_system");
+        a.setPassword("Efika@viv0Gvt");
+
+        return a;
+    }
+
 }
