49ab83789567e793a2bfbfad50a435338d7b8e8c
henmerlin <henmerlin@gmail.com>
2017-05-04 15:57:32 -0300

    Adição do Regex replace (trativa falha no retorno da API).

--- JavaSource/util/GsonUtil.java
+++ JavaSource/util/GsonUtil.java
@@ -8,6 +8,7 @@ package util;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import java.io.StringReader;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -18,13 +19,20 @@ public class GsonUtil {
     private static Gson gson = new Gson();
 
     public static Object convert(String str, Class c) {
-        JsonReader reader = new JsonReader(new StringReader(str));
+        JsonReader reader = new JsonReader(new StringReader(parseFakeBoolean(str)));
         reader.setLenient(true);
         return gson.fromJson(reader, c);
     }
 
     public static String serialize(Object ob) {
         return gson.toJson(ob, ob.getClass());
+    }
+
+    public static String parseFakeBoolean(String content) {
+        Pattern pYes = Pattern.compile("(\"On\"|\"Yes\"|\"true\")", Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+        String yes = pYes.matcher(content).replaceAll("true");
+        Pattern pNo = Pattern.compile("(\"Off\"|\"No\"|\"false\")", Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+        return pNo.matcher(yes).replaceAll("false");
     }
 
 }
